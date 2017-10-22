62ee4acbb7ecac451e22753b1ff2e71c89bb95a0
Anders Hagward <taperlawyer@gmail.com>
2017-10-22 20:53:39 +0200

    Add function for retrieving the latest games

--- server/db.js
+++ server/db.js
@@ -23,6 +23,29 @@ class Database {
     );
   }
 
+  getLatestGames(n) {
+    return this.query((db, resolve, reject) =>
+      db.all(
+        `
+        SELECT
+          g.id,
+          g.created_timestamp,
+          p1.name AS player_a_name,
+          p2.name AS player_b_name,
+          g.player_a_score,
+          g.player_b_score
+        FROM game AS g
+        INNER JOIN player AS p1 ON p1.id = player_a_id
+        INNER JOIN player AS p2 ON p2.id = player_b_id
+        ORDER BY created_timestamp DESC
+        LIMIT (?)
+        `,
+        n,
+        (error, rows) => error ? reject(error) : resolve (rows)
+      )
+    );
+  }
+
   query(callback) {
     return new Promise((resolve, reject) => {
       const db = new sqlite3.Database(this.databaseName);
