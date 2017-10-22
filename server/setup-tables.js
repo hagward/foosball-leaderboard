d376ce039e5feed678a46daa850d4729965aef18
Anders Hagward <taperlawyer@gmail.com>
2017-10-22 17:07:36 +0200

    Add db setup script

---
+++ server/setup-tables.js
@@ -0,0 +1,28 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('foosball.db');
+
+db.serialize(() => {
+  db.run('DROP TABLE IF EXISTS player');
+  db.run('DROP TABLE IF EXISTS game');
+
+  db.run(`
+    CREATE TABLE player (
+      id INTEGER PRIMARY KEY NOT NULL,
+      name TEXT UNIQUE NOT NULL,
+      rating INTEGER NOT NULL
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE game (
+      id INTEGER PRIMARY KEY NOT NULL,
+      created_datetime INTEGER NOT NULL,
+      player_a_id INTEGER NOT NULL,
+      player_b_id INTEGER NOT NULL,
+      player_a_score INTEGER NOT NULL,
+      player_b_score INTEGER NOT NULL,
+      FOREIGN KEY(player_a_id) REFERENCES player(id),
+      FOREIGN KEY(player_b_id) REFERENCES player(id)
+    )
+  `);
+});
