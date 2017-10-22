381476c92efab50dd2ebde0b891482bc073f0adc
Anders Hagward <taperlawyer@gmail.com>
2017-10-22 20:30:32 +0200

    Add query function

--- server/db.js
+++ server/db.js
@@ -6,27 +6,27 @@ class Database {
   }
 
   addPlayer(name) {
-    return new Promise((resolve, reject) => {
-      const db = new sqlite3.Database(this.databaseName);
-
+    return this.query((db, resolve, reject) => {
       const statement = db
         .prepare('INSERT INTO player (name) VALUES (?)')
         .run(name, error => error ? reject(error) : resolve());
       statement.finalize();
-
-      db.close();
     });
   }
 
   getPlayers() {
-    return new Promise((resolve, reject) => {
-      const db = new sqlite3.Database(this.databaseName);
-
+    return this.query((db, resolve, reject) =>
       db.all(
         'SELECT id, name FROM player ORDER BY name',
         (error, rows) => error ? reject(error) : resolve(rows)
-      );
+      )
+    );
+  }
 
+  query(callback) {
+    return new Promise((resolve, reject) => {
+      const db = new sqlite3.Database(this.databaseName);
+      callback(db, resolve, reject);
       db.close();
     });
   }
