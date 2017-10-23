bdbc366c027b5fd4acb57c360d51b8cc3998a76a
Anders Hagward <taperlawyer@gmail.com>
2017-10-23 23:11:18 +0200

    Use Elo k and F from Bonzini

--- server/elo.js
+++ server/elo.js
@@ -5,20 +5,14 @@ class Elo {
   }
 
   pointsChange(goalDifference, ratingDifference) {
-    const g = this.g(Math.abs(goalDifference));
+    const k = this.k();
     const result = this.result(goalDifference);
     const expected = this.expected(ratingDifference);
-    return this.p(20, g, result, expected);
+    return this.p(k, result, expected);
   }
 
-  g(goalDifference) {
-    if (goalDifference <= 1) {
-      return 1;
-    } else if (goalDifference === 2) {
-      return 3/2;
-    } else {
-      return (11 + goalDifference)/8;
-    }
+  k() {
+    return 50;
   }
 
   result(goalDifference) {
@@ -32,11 +26,11 @@ class Elo {
   }
 
   expected(ratingDifference) {
-    return 1/(Math.pow(10, -ratingDifference/400) + 1);
+    return 1/(Math.pow(10, -ratingDifference/1000) + 1);
   }
 
-  p(k, g, result, expected) {
-    return k * g * (result - expected);
+  p(k, result, expected) {
+    return k * (result - expected);
   }
 }
 
--- server/elo.test.js
+++ server/elo.test.js
@@ -1,37 +1,11 @@
 const elo = require('./elo');
 
-// Examples taken from https://en.wikipedia.org/wiki/World_Football_Elo_Ratings#Examples
-
-describe('pointsChange', () => {
-  test('Team A (630) vs. Team B (500) - 3:1', () => {
-    expect(elo.pointsChange(2, 130)).toBeCloseTo(9.64);
-    expect(elo.pointsChange(-2, -130)).toBeCloseTo(-9.64);
-  });
-
-  test('Team A (630) vs. Team B (500) - 1:3', () => {
-    expect(elo.pointsChange(-2, 130)).toBeCloseTo(-20.36);
-    expect(elo.pointsChange(2, -130)).toBeCloseTo(20.36);
-  });
-
-  test('Team A (630) vs. Team B (500) - 2:2', () => {
-    expect(elo.pointsChange(0, 130)).toBeCloseTo(-3.58);
-    expect(elo.pointsChange(0, -130)).toBeCloseTo(3.58);
-  });
-});
-
 describe('newRatings', () => {
-  test('Team B (500) vs. Team C (480) - 3:1', () => {
-    expect(elo.newRatings(3, 1, 500, 480)).toEqual([514, 466]);
-    expect(elo.newRatings(1, 3, 480, 500)).toEqual([466, 514]);
+  test('Player A (1800) beats Player B (1550)', () => {
+    expect(elo.newRatings(3, 1, 1800, 1550)).toEqual([1818, 1532]);
   });
 
-  test('Team B (500) vs. Team C (480) - 1:3', () => {
-    expect(elo.newRatings(1, 3, 500, 480)).toEqual([484, 496]);
-    expect(elo.newRatings(3, 1, 480, 500)).toEqual([496, 484]);
-  });
-
-  test('Team B (500) vs. Team C (480) - 2:2', () => {
-    expect(elo.newRatings(2, 2, 500, 480)).toEqual([499, 481]);
-    expect(elo.newRatings(2, 2, 480, 500)).toEqual([481, 499]);
+  test('Player A (1800) loses to Player B (1550)', () => {
+    expect(elo.newRatings(1, 3, 1800, 1550)).toEqual([1768, 1582]);
   });
 });
