ddabbcb14cb5b331118fb37d2b3a475e60ea1e54
Anders Hagward <taperlawyer@gmail.com>
2017-11-02 20:42:09 +0100

    Don't drop tables in setup script

--- server/setup-tables.js
+++ server/setup-tables.js
@@ -2,11 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('foosball.db');
 
 db.serialize(() => {
-  db.run('DROP TABLE IF EXISTS player');
-  db.run('DROP TABLE IF EXISTS game');
-
   db.run(`
-    CREATE TABLE player (
+    CREATE TABLE IF NOT EXISTS player (
       id INTEGER PRIMARY KEY NOT NULL,
       name TEXT UNIQUE NOT NULL,
       rating INTEGER NOT NULL DEFAULT 1500
@@ -14,7 +11,7 @@ db.serialize(() => {
   `);
 
   db.run(`
-    CREATE TABLE game (
+    CREATE TABLE IF NOT EXISTS game (
       id INTEGER PRIMARY KEY NOT NULL,
       created_timestamp INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
       player_a_id INTEGER NOT NULL,
