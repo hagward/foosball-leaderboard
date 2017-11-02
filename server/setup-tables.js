348b3c8fd9b4a41b84155fb7d94dbf03edc0bb71
Anders Hagward <taperlawyer@gmail.com>
2017-11-02 20:55:58 +0100

    Add team tables

--- server/setup-tables.js
+++ server/setup-tables.js
@@ -11,6 +11,24 @@ db.serialize(() => {
   `);
 
   db.run(`
+    CREATE TABLE IF NOT EXISTS team (
+      id INTEGER PRIMARY KEY NOT NULL,
+      name TEXT UNIQUE NOT NULL,
+      rating INTEGER NOT NULL DEFAULT 1500
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS player_team (
+      player_id INTEGER NOT NULL,
+      team_id INTEGER NOT NULL,
+      PRIMARY KEY(player_id, team_id),
+      FOREIGN KEY(player_id) REFERENCES player(id),
+      FOREIGN KEY(team_id) REFERENCES team(id)
+    )
+  `);
+
+  db.run(`
     CREATE TABLE IF NOT EXISTS game (
       id INTEGER PRIMARY KEY NOT NULL,
       created_timestamp INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
